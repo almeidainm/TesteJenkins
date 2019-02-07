87f36f864cde980e236a4d21a7b58dbc6040e0f3
Felipe <felipealmeidaw3@gmail.com>
2019-02-07 15:34:30 -0200

    Aplicação para teste no Jenkins

---
+++ GoogleSteps.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System;
+using TechTalk.SpecFlow;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using NUnit.Framework;
+
+
+namespace TesteGoogleJenkins
+{
+    [Binding]
+    public class AcessaOGoogleEVerificaSeABuscaRetornouComSucessoSteps
+    {
+        IWebDriver driver_ = new ChromeDriver("C:\\ChromeDriver\\");
+
+        [Given(@"que eu acesse o Google")]
+        public void DadoQueEuAcesseOGoogle()
+        {   
+            //System.Threading.Thread.Sleep(5000);
+            driver_.Navigate().GoToUrl("https://www.google.com.br");
+        }
+        
+        [Given(@"digite ""(.*)"" na busca")]
+        public void DadoDigiteNaBusca(string p0)
+        {
+            driver_.FindElement(By.XPath("//input[@title='Pesquisar']")).Click();
+            driver_.FindElement(By.XPath("//input[@title='Pesquisar']")).SendKeys("Inmetrics");
+        }
+        
+        [When(@"clico no botão buscar")]
+        public void QuandoClicoNoBotaoBuscar()
+        {
+            driver_.FindElement(By.XPath("//input[@title='Pesquisar']")).SendKeys(Keys.Enter);  
+        }
+        
+        [Then(@"verifico se o site da inmetrics voltou na busca")]
+        public void EntaoVerificoSeOSiteDaInmetricsVoltouNaBusca()
+        {
+            string valoratual = driver_.FindElement(By.XPath("//h3[contains(text(),'Inmetrics: Home')]")).Text;
+            valoratual.Contains("Inmetrics: Home");
+            driver_.FindElement(By.XPath("//h3[contains(text(),'Inmetrics: Home')]")).Click();
+        }
+    }
+}
---
+++ TesteGoogleJenkins/SpecFlowFeature1.feature
@@ -0,0 +1,9 @@
+﻿#language: pt-br
+Funcionalidade: Acessa o Google e verifica se a busca retornou com sucesso
+
+
+Cenário: Acessar o Google
+	Dado que eu acesse o Google
+	E digite "Inmetrics" na busca
+	Quando clico no botão buscar
+	Então verifico se o site da inmetrics voltou na busca
